976cba1a66c5b3fa3f1eb7a0dda1a6f24c01abcb
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-28 14:43:53 -0500

    Update main.tf cluster access

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -27,7 +27,8 @@ module "vpc" {
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
-
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = true
   cluster_name    = "hello-eks"
   cluster_version = "1.29"
   subnet_ids      = module.vpc.public_subnet_ids
