e3bbc58b4d3a0a913036c3b5891fce06ce810282
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-28 15:34:00 -0500

    Update aws_auth.tf nuevo

--- terraform/eks/aws_auth.tf
+++ terraform/eks/aws_auth.tf
@@ -1,16 +1,22 @@
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapUsers: |
+    - userarn: arn:aws:iam::585768155983:user/terraform-user
+      username: terraform-user
+      groups:
+        - system:masters
+
+resource "null_resource" "apply_aws_auth" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f aws_auth.yaml"
+    environment = {
+      KUBECONFIG = "~/.kube/config"
+    }
   }
 
-  data = {
-    mapUsers = yamlencode([
-      {
-        userarn  = "arn:aws:iam::585768155983:user/terraform-user"
-        username = "terraform-user"
-        groups   = ["system:masters"]
-      }
-    ])
-  }
+  depends_on = [module.eks]
 }
