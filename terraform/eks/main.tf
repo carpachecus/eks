a7a4a0ae7e649028b0879288bc3b0970b729641c
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-29 00:33:57 -0500

    Update main.tf nuevo tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,49 +1,40 @@
-terraform {
-  required_version = ">= 1.4.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
-
 
 module "vpc" {
-  source                = "../vpc"
-  region                = var.region
-  vpc_name              = "hello-vpc"
-  vpc_cidr              = "10.0.0.0/16"
-  public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
-  azs                   = ["us-east-1a", "us-east-1b"]
-}
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
 
+  name = "hello-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "hello-vpc"
+  }
+}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
-  cluster_endpoint_public_access = true
-  cluster_endpoint_private_access = true
+
   cluster_name    = "hello-eks"
   cluster_version = "1.29"
-  subnet_ids      = module.vpc.public_subnet_ids
-  vpc_id          = module.vpc.vpc_id
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnet_ids
 
   enable_irsa = true
-
-  manage_aws_auth_configmap = true
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::585768155983:user/terraform-user"
-      username = "terraform-user"
-      groups   = ["system:masters"]
-    }
-  ]
 
   eks_managed_node_group_defaults = {
     instance_types = ["t3.medium"]
@@ -54,9 +45,30 @@ module "eks" {
       min_size     = 1
       max_size     = 3
       desired_size = 2
-
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
     }
   }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+module "aws_auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "20.8.4"
+
+  depends_on = [module.eks]
+
+  eks_cluster_id = module.eks.cluster_name
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::585768155983:user/terraform-user"
+      username = "terraform-user"
+      groups   = ["system:masters"]
+    }
+  ]
 }
