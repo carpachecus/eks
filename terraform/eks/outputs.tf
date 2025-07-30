ac91e8110ad4b423f426dabbc4e26a92743d8d2b
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-30 11:33:51 -0500

    Update outputs.tf out

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -1,11 +1,15 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
 }
 
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "kubeconfig_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+output "cluster_name" {
+  value = module.eks.cluster_name
 }
