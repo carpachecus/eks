7ab160527b4a4e3357e3d45c6f2ab5025f98bf97
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-30 14:54:22 -0500

    Update main.tf sin aws

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -51,18 +51,3 @@ module "eks" {
 }
 
 
-module "aws_auth" {
-  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
-  version = "20.8.4"
-
-  cluster_name                 = module.eks.cluster_name
-  manage_aws_auth_configmap   = true
-
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::585768155983:user/terraform-user"
-      username = "terraform-user"
-      groups   = ["system:masters"]
-    }
-  ]
-}
