ba01e47577e7b80a24e1e33bb923ed596b1f8542
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-30 11:33:12 -0500

    Update variables.tf

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,28 +1,15 @@
-variable "aws_region" {
+variable "region" {
   default = "us-east-1"
-}
-
-variable "vpc_name" {
-  default = "hello-vpc"
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "azs" {
-  default = ["us-east-1a", "us-east-1b"]
-}
-
-variable "public_subnet_cidrs" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "cluster_name" {
-  default = "hello-eks"
 }
 
 variable "environment" {
   default = "dev"
 }
 
+variable "cluster_name" {
+  default = "hello-eks"
+}
+
+variable "cluster_version" {
+  default = "1.29"
+}
