8a6d60ba7029f7f386bb84652b28ec7f8bfb796f
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-30 11:14:29 -0500

    Update main.tf nuevo

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -13,11 +24,14 @@ module "vpc" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
 
   tags = {
-    Name = "hello-vpc"
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
 
@@ -25,18 +39,14 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name    = "hello-eks"
-  cluster_version = "1.29"
-
+  cluster_name                    = "hello-eks"
+  cluster_version                 = "1.29"
+  vpc_id                          = module.vpc.vpc_id
+  subnet_ids                      = module.vpc.private_subnet_ids
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
-
-
   enable_irsa = true
-
   authentication_mode = "API"
   enable_cluster_creator_admin_permissions = true
 
@@ -59,4 +69,3 @@ module "eks" {
     Terraform   = "true"
   }
 }
-
