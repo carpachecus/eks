8eaeb39a0054f55586b6953ea9ab4353c9f3c136
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-30 11:32:27 -0500

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,16 +1,5 @@
-terraform {
-  required_version = ">= 1.4.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 module "vpc" {
@@ -22,24 +11,14 @@ module "vpc" {
 
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  enable_dns_hostnames   = true
-  enable_dns_support     = true
-
- # vpc/outputs.tf
-  output "vpc_id" {
-  value = module.vpc.vpc_id
-  }
-
-  output "public_subnet_ids" {
-  value = module.vpc.public_subnets
-  }
+  enable_nat_gateway = false
+  single_nat_gateway = false
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment
     Terraform   = "true"
   }
 }
@@ -48,34 +27,15 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name                    = "hello-eks"
-  cluster_version                 = "1.29"
-  vpc_id                          = module.vpc.vpc_id
-  subnet_ids                      = module.vpc.public_subnet_ids
-
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  subnet_ids      = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 
   enable_irsa = true
-  authentication_mode = "API"
-  enable_cluster_creator_admin_permissions = true
-
-  eks_managed_node_group_defaults = {
-    instance_types = ["t3.medium"]
-  }
-
-  eks_managed_node_groups = {
-    default = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
-    }
-  }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment
     Terraform   = "true"
   }
 }
