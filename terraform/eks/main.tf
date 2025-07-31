444c66870128ac936b2189a4d9d710b05c246c07
carpachecus <34517605+carpachecus@users.noreply.github.com>
2025-07-30 20:57:22 -0500

    Update main.tf load

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -10,7 +10,7 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+ 
 }
 
 module "vpc" {
